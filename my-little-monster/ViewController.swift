979efab5711331f7a80094cc5e7d7a8622b781df
sslack3130 <underwood.dj.steve@gmail.com>
2016-01-09 12:47:45 +0000

    Initial Commit

---
+++ my-little-monster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-little-monster
+//
+//  Created by Steve Slack on 09/01/2016.
+//  Copyright © 2016 Steve Slack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
